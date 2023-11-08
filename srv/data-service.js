739ef6833bb7d4e55b34f5f41a2a905bc50688e6
Trainer CAPM <saptrainercapm@gmail.com>
2023-11-08 13:16:37 +0000

    branch made

--- srv/data-service.js
+++ srv/data-service.js
@@ -1,34 +1,28 @@
 module.exports = srv => {
-    srv.on("READ", "Employees", req => {
+    // srv.on("READ","Employees", req => {
+    //     console.log("Testing");
+    //     console.log(req.data);
+    //     const { Employees } = cds.entities;
+    //     const tx = cds.transaction(req);
+    //     if (req.data.Empid) {
+    //         return tx.run(SELECT.from(Employees).where({ Empid: req.data.Empid }));
+    //     }
+    //     else {
+    //         return tx.run(SELECT.from(Employees));
+    //     }
+    // });
+    srv.after("READ", "Employees", rs => {
         console.log(" Testing");
-        console.log(req.data);
-        const { Employees } = cds.entities;
-        const tx = cds.transaction(req);
-        if (req.data.Empid) {
-            return tx.run(SELECT.from(Employees).where({ Empid: req.data.Empid }));
+        console.log(rs);
+        for (var i = 0; i < rs.length; i++)
+        {
+            if (rs[i].empDesg == "Team Lead") {
+                rs[i].empDesg = rs[i].empDesg + " (TL)";
+            }
+            else if (rs[i].empDesg == "Project Manager") {
+                rs[i].empDesg = rs[i].empDesg + "(PM)";
+            }
         }
-        else {
-            return tx.run(SELECT.from(Employees));
-        }
+        return rs;
     });
-//     srv.after("READ", "Employees", rs=>
-//     {
-//         console.log(" Testing");
-//         console.log(rs);
-//         for (var i=0;i<rs.length; i++)
-//         {
-//         if(rs[i].Desig== "Team Lead"){
-//                  rs[i].Desig = rs[i].Desig+" (TL)";
-//         }
-//         else if(rs[i].Desig== "Senior Software Engineer")
-//         { 
-//             rs[i].Desig= rs[i].Desig+" (SSE)";
-//         }
-//         }
-//         return rs;
-
-
-// });
-
-
 }
