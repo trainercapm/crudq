621a7de02480026da29b93261cf6e4a04e325da3
Trainer CAPM <saptrainercapm@gmail.com>
2023-11-09 01:29:48 +0000

    CRUD handlers

--- srv/data-service.js
+++ srv/data-service.js
@@ -1,4 +1,5 @@
 module.exports = srv => {
+
     // srv.on("READ","Employees", req => {
     //     console.log("Testing");
     //     console.log(req.data);
@@ -11,11 +12,11 @@ module.exports = srv => {
     //         return tx.run(SELECT.from(Employees));
     //     }
     // });
+    
     srv.after("READ", "Employees", rs => {
-        console.log(" Testing");
+        console.log("Read Testing");
         console.log(rs);
-        for (var i = 0; i < rs.length; i++)
-        {
+        for (var i = 0; i < rs.length; i++) {
             if (rs[i].empDesg == "Team Lead") {
                 rs[i].empDesg = rs[i].empDesg + " (TL)";
             }
@@ -24,5 +25,47 @@ module.exports = srv => {
             }
         }
         return rs;
+    });
+
+    // srv.before("CREATE", "Employees", req => {
+    //     console.log("Create Testing");
+    //     console.log(req.data);
+    //     if (req.data.Status === "Permenant"){ 
+    //         req.data.StatusCriticality = 3;
+    //     }else{
+    //     req.data.StatusCriticality = 1;
+    //     }
+
+    // });
+
+    srv.on("CREATE", "Employees", req => {
+        if (req.data.Status === "Permenant"){ 
+                    req.data.StatusCriticality = "PRIO-1";
+                }else{
+                req.data.StatusCriticality = "PRIO-2";
+                }
+        const Employees = cds.entities;
+        const tx = cds.transaction(req)
+        return tx.run(INSERT.into(Employees)
+            .columns('empId', 'empName', 'empDesg', 'empAdd', 'empSalary', 'empPos')
+            .values(req.data.empId,
+                req.data.empName,
+                req.data.empDesg,
+                req.data.empAdd,
+                req.data.empSalary,
+                req.data.empPos));
+
+    });
+
+    srv.on("UPDATE", "Employees", async req => {
+        const Employees = cds.entities;
+        const tx = cds.transaction(req)
+        return await tx.run(UPDATE(Employees).set({empPos:req.data.empPos}).where({empId:req.data.empId}));
+    });
+
+    srv.on("DELETE", "Employees", async req => {
+        const Employees = cds.entities;
+        const tx = cds.transaction(req)
+        return await tx.run(DELETE(Employees).where({empId:req.data.empId}));
     });
 }
